b2e70784de3cebed97b64d683b4566add08244fa
Tan Li Boon <undisputed.seraphim@gmail.com>
2023-01-26 01:00:48 +0900

    python: Update code to Python 3

--- ast/cxx_visitor.py
+++ ast/cxx_visitor.py
@@ -11,7 +11,7 @@ class Printer(object):
     pass
 
   def start_file(self):
-    print C_LICENSE_COMMENT + '''/** @generated */
+    print(C_LICENSE_COMMENT + '''/** @generated */
 
 #pragma once
 
@@ -26,27 +26,28 @@ class AstVisitor {
 public:
   virtual ~AstVisitor() {}
 '''
+    )
 
   def end_file(self):
-    print '};' # end AstVisitor
-    print
-    print '}'
-    print '}'
-    print '}'
-    print '}'
+    print('};') # end AstVisitor
+    print()
+    print('}')
+    print('}')
+    print('}')
+    print('}')
 
   def start_type(self, name):
     titleName = title(name)
     camelName = camel(titleName)
-    print '  virtual bool visit%s(const %s &%s) { return true; }' % (
+    print('  virtual bool visit%s(const %s &%s) { return true; }' % (
       titleName,
       titleName,
-      camelName)
-    print '  virtual void endVisit%s(const %s &%s) { }' % (
+      camelName))
+    print ('  virtual void endVisit%s(const %s &%s) { }' % (
       titleName,
       titleName,
-      camelName)
-    print
+      camelName))
+    print()
 
   def end_type(self, name):
     pass
